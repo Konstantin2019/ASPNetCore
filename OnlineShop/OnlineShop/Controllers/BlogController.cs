31263ebb1cd9d57cd73decb789f83719a838e45d
konst_g_p <konst_g_p@mail.ru>
2021-01-22 17:37:18 +0300

    Созданы все нобходимые контроллеры и вьюхи сайта, реализована навигация главного меню. Декомпозиция html разметки для всех страниц, кроме Index будет произведена позже

---
+++ OnlineShop/OnlineShop/Controllers/BlogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index() => View("Blog");
+        public IActionResult Single() => View("Single");
+    }
+}
---
+++ OnlineShop/OnlineShop/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index() => View("Products");
+        public IActionResult Details() => View("Details");
+        public IActionResult Checkout() => View("Checkout");
+        public IActionResult Cart() => View("Cart");
+        public IActionResult Login() => View("Login");
+    }
+}
