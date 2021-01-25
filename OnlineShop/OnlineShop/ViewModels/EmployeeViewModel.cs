59bb7d66e2b8b277925a43119e112a4dab428598
konst_g_p <konst_g_p@mail.ru>
2021-01-25 22:52:14 +0300

    Добавлены views Update и Delete, переработана view Details по стандартной форме, добавлен слой _EmployeeLayout, реализован фукционал изменения и удаления сотрудников

--- OnlineShop/OnlineShop/ViewModels/EmployeeViewModel.cs
+++ OnlineShop/OnlineShop/ViewModels/EmployeeViewModel.cs
@@ -7,12 +7,12 @@ namespace OnlineShop.ViewModels
 {
     public class EmployeeViewModel
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string SurName { get; set; }
-        public string Patronymic { get; set; }
-        public byte Age { get; set; }
-        public string Address { get; set; }
-        public string Email { get; set; }
+        public int Id { get; init; }
+        public string FirstName { get; init; }
+        public string SurName { get; init; }
+        public string Patronymic { get; init; }
+        public byte Age { get; init; }
+        public string Address { get; init; }
+        public string Email { get; init; }
     }
 }
