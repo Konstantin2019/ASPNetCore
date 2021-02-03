54a2e203b9a56d68db4ab6114bd7cc0d0fa97b9a
konst_g_p <konst_g_p@mail.ru>
2021-02-03 19:38:24 +0300

    Добавлены view регистрации и входа, написан метод Register контроллера

--- OnlineShop/OnlineShop/Controllers/AccountController.cs
+++ OnlineShop/OnlineShop/Controllers/AccountController.cs
@@ -1,17 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Domain.Models;
+using OnlineShop.ViewModels;
 using System.Threading.Tasks;
 
 namespace OnlineShop.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Login() => View();
 
         [HttpGet]
-        public IActionResult Register() => View();
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel model) 
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var user = new User 
+            {
+                UserName = model.UserName 
+            };
+            var request = await userManager.CreateAsync(user, model.Password);
+
+            if (request.Succeeded)
+            {
+                await signInManager.SignInAsync(user, false);
+                RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach (var identityError in request.Errors)
+                {
+                    ModelState.AddModelError("", identityError.Description);
+                }
+            }
+
+            return View(model);
+        }
     }
 }
