8719c9ce8182433a34e881ebd432beefc72241e8
konst_g_p <konst_g_p@mail.ru>
2021-02-11 20:50:35 +0300

    Добавлены модели и вью модели корзины

---
+++ OnlineShop/OnlineShop/ViewModels/CartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineShop.ViewModels
+{
+    public class CartViewModel
+    {
+        public IEnumerable<(ProductViewModel product, int quantity)> Items { get; set; }
+        public int ItemsCount => Items?.Sum(i => i.quantity) ?? 0;
+        public decimal TotalPrice => Items?.Sum(i => i.product.Price * i.quantity) ?? 0;
+    }
+}
