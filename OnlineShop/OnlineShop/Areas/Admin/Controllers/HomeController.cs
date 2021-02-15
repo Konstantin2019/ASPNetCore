f3c527fcac281d49f9eaca4ee481b6e81b9bdaac
konst_g_p <konst_g_p@mail.ru>
2021-02-15 21:26:52 +0300

    Добавлены контроллер и представление товаров

--- OnlineShop/OnlineShop/Areas/Admin/Controllers/HomeController.cs
+++ OnlineShop/OnlineShop/Areas/Admin/Controllers/HomeController.cs
@@ -1,27 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using OnlineShop.Domain.Models;
 using OnlineShop.Domain.Models.Identity;
-using OnlineShop.Services.Interfaces;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
     [Area("Admin"), Authorize(Roles = Role.administrator)]
     public class HomeController : Controller
     {
-        private readonly IProductService productService;
-
-        public HomeController(IProductService productService)
-        {
-            this.productService = productService;
-        }
         public IActionResult Index() => View();
-
-        public IActionResult ProductList()
-        {
-            var products = productService.GetProducts(new ProductFilter());
-            return View(products);
-        }
-
     }
 }
---
+++ OnlineShop/OnlineShop/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Domain.Models;
+using OnlineShop.Domain.Models.Identity;
+using OnlineShop.Services.Interfaces;
+
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize(Roles = Role.administrator)]
+    public class ProductsController : Controller
+    {
+        private readonly IProductService productService;
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+        public IActionResult Index()
+        {
+            var products = productService.GetProducts(new ProductFilter());
+            return View(products);
+        }
+    }
+}
